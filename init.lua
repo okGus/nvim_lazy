102f85956f01d19aa75b71b1b5ad983e70eb573a
okGus <rosalesagustavo@gmail.com>
2025-04-07 11:01:39 -0400

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,9 @@
+--require("supersmol")
+-- Load options (set.lua equivalent)
+require("config.options")
+
+-- Load keymaps (remap.lua equivalent)
+require("config.keymaps")
+
+-- init my lazy.nvim
+require("config.lazy")
